7103ea9bf1daced0fd5cd70a69a3368c4bd902c6
Pratyaksh Agarwal <pratyaksh.agrwl@gmail.com>
2025-11-30 19:28:05 +0530

    added " and \ handling inside double quotes

--- src/main.rs
+++ src/main.rs
@@ -142,46 +142,57 @@ fn tokenizer(input: &str) -> Vec<String> {
     let mut current = String::new();
     let mut in_single_quotes = false;
     let mut in_double_quotes = false;
-    let mut escaped  = false;
-
+    let mut escaped = false;
 
     let mut chars = input.chars().peekable();
 
     while let Some(ch) = chars.next() {
-
         if escaped {
-            current.push(ch);
-            escaped = false;
-            continue;
+
+            //handling of * and \ after \ (we arfe ignoring both two and apart from these two if anything occuers after \ we not use any special proiperty of \ hence we will include \ in our token also)
+            if in_double_quotes {
+                match ch {
+                    '"' | '\\' => current.push(ch),
+                    _ => {
+                        current.push('\\');
+                        current.push(ch);
+                    }
+                }
+
+                escaped = false;
+                continue;
+            } else {
+                current.push(ch);
+
+                escaped = false;
+                continue;
+            }
         }
 
         match ch {
-
             //this case only added to remove \ and ignore the usecase of special char after the \ in above if statement
             '\\' => {
-                if !in_single_quotes && !in_double_quotes {
+                if !in_single_quotes {
                     escaped = true;
                     continue;
-                }else {
+                } else {
                     current.push(ch);
                 }
-
             }
 
             //case : if ' then set is single wuotes to true/false
             '\'' => {
                 if !in_double_quotes {
                     in_single_quotes = !in_single_quotes;
-                }else {
+                } else {
                     current.push(ch);
                 }
             }
 
-
-            '"' =>{
+            '"' => {
                 if !in_single_quotes {
                     in_double_quotes = !in_double_quotes;
-                }else {
+                } else {
                     current.push(ch);
                 }
             }
