c6f897f7fff4121f3e9dbfd42cc47ac466dbe40b
Pratyaksh Agarwal <pratyaksh.agrwl@gmail.com>
2025-11-30 13:04:31 +0530

    execute any external program/command present in path

--- src/main.rs
+++ src/main.rs
@@ -5,69 +5,80 @@ use std::os::unix::fs::PermissionsExt;
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
 
-    loop{
-
-        
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        
+
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-      
-
-        let command  = command.trim();
-
+        let command = command.trim();
 
         //this will break the command into arrya of tokens
         let tokens = command.split_whitespace().collect::<Vec<&str>>();
 
-        match tokens[0]{
+        match tokens[0] {
             "exit" => break,
             "echo" => println!("{}", tokens[1..].join(" ")),
             // "type" => println!("{} is a shell builtin", tokens[1..].join(" ")),
             "type" => {
                 let arg = tokens[1];
                 match arg {
-                    "exit"|"echo"|"type" =>{
-                        println!("{} is a shell builtin",arg);
-                    },
-                    _ =>{
-
+                    "exit" | "echo" | "type" => {
+                        println!("{} is a shell builtin", arg);
+                    }
+                    _ => {
                         //displays a long string of path variables like "/home/pratyaksh/.bun/bin:/home/pratyaksh/.local/share/solana/install/active_release/bin:/home/pratyaksh/.local/bin:/home/pratyaksh/bin:/home/etcccccccc"
                         let path_var = std::env::var("PATH").unwrap();
                         let mut found = false;
 
                         for path_dir in path_var.split(':') {
-                            
                             let full_path = std::path::Path::new(path_dir).join(tokens[1]);
                             // println!("{:?}",full_path);
                             if full_path.exists() {
-
-                                if let Ok(metadata) = std::fs::metadata(&full_path){
+                                if let Ok(metadata) = std::fs::metadata(&full_path) {
                                     if metadata.permissions().mode() & 0o111 != 0 {
                                         println!("{} is {}", tokens[1], full_path.display());
                                         found = true;
                                         break;
                                     }
                                 }
-
                             }
-
                         }
-                        
+
                         if !found {
                             println!("{}: not found", tokens[1]);
                         }
-                    
-                    
                     }
                 }
             }
-            _ =>  println!("{}: command not found",command)
-        } 
-       
+            _ => {
+                let args = &tokens[1..];
+
+                //if command is not build in , first we will find the command in path and then check if it exits, then execute
+                let path = std::env::var("PATH").unwrap();
+                let paths = path.split(':');
+                let mut found = false;
+
+                for path in paths {
+                    let full_path = format!("{}/{}", path, tokens[0]);
+
+                    if std::fs::metadata(&full_path).is_ok() {
+                        let mut process = std::process::Command::new(tokens[0])
+                            .args(args)
+                            .spawn()
+                            .unwrap();
+
+                        //waiting for the process to complete
+                        let _status = process.wait().unwrap();
+                        found = true;
+                    }
+                }
+
+                if !found {
+                    println!("{}: command not found", command);
+                }
+            }
+        }
     }
-
-
 }
