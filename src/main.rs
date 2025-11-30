82f9fef29b1f2490eccfdd712b7f9c9c2d16894c
Pratyaksh Agarwal <pratyaksh.agrwl@gmail.com>
2025-11-30 11:43:53 +0530

    fixed type command

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,27 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
+      
+
         let command  = command.trim();
 
+
+        //this will break the command into arrya of tokens
         let tokens = command.split_whitespace().collect::<Vec<&str>>();
-        // println!("{:?}",iter.next());
 
         match tokens[0]{
             "exit" => break,
             "echo" => println!("{}", tokens[1..].join(" ")),
-            "type" => println!("{} is a shell builtin", tokens[1..].join(" ")),
+            // "type" => println!("{} is a shell builtin", tokens[1..].join(" ")),
+            "type" => {
+                let arg = tokens[1];
+                match arg {
+                    "exit"|"echo"|"type" =>{
+                        println!("{} is a shell builtin",arg);
+                    },
+                    _ => println!("{}: not found",tokens[1..].join(" "))
+                }
+            }
             _ =>  println!("{}: command not found",command)
         } 
        
